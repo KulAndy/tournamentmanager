11d2fe0db59d8f07c80927a0b9224e390e8fe657
Andrzej Kulesza <andykrk22@gmail.com>
2023-10-15 20:58:20 +0200

    fix pairing comparing

--- src/main/java/com/example/lolmanager/comparator/PairingComparator.java
+++ src/main/java/com/example/lolmanager/comparator/PairingComparator.java
@@ -64,34 +64,42 @@ public class PairingComparator implements Comparator<Game>, Serializable {
         } else if (Objects.equals(black2.getName(), "unpaired") && !(Objects.equals(black1.getName(), "unpaired"))) {
             return -1;
         } else {
+            int roundNo = Integer.min(white1.getRounds().indexOf(o1), white2.getRounds().indexOf(o2));
             float maxResult1 = Float.max(
-                    white1.getPoints(),
-                    black1.getPoints()
+                    white1.getPointInRound(roundNo),
+                    black1.getPointInRound(roundNo)
             );
             float maxResult2 = Float.max(
-                    white2.getPoints(),
-                    black2.getPoints()
+                    white2.getPointInRound(roundNo),
+                    black2.getPointInRound(roundNo)
             );
             if (maxResult1 != maxResult2) {
-                return Float.compare(maxResult1, maxResult2);
+                return -Float.compare(maxResult1, maxResult2);
             } else {
-                int minStr1 = Integer.min(
-                        playersObs.contains(white1) ? playersObs.indexOf(white1) : Integer.MAX_VALUE,
-                        playersObs.contains(black1) ? playersObs.indexOf(black1) : Integer.MAX_VALUE
-                );
-                int minStr2 = Integer.min(
-                        playersObs.contains(white2) ? playersObs.indexOf(white2) : Integer.MAX_VALUE,
-                        playersObs.contains(black2) ? playersObs.indexOf(black2) : Integer.MAX_VALUE
-                );
-                if (minStr1 != minStr2) {
-                    return Integer.compare(minStr1, minStr2);
-                } else {
-                    int higherPlayer1 = black1.getFideRating() > white1.getFideRating() ? playersObs.indexOf(black1) : playersObs.indexOf(white1);
-                    int higherPlayer2 = black2.getFideRating() > white2.getFideRating() ? playersObs.indexOf(black2) : playersObs.indexOf(white2);
+                float sumResult1 = white1.getPointInRound(roundNo) + black1.getPointInRound(roundNo);
+                float sumResult2 = white2.getPointInRound(roundNo) + black2.getPointInRound(roundNo);
+                if(sumResult1 != sumResult2){
+                    return -Float.compare(sumResult1, sumResult2);
+                }else{
+                    int minStr1 = Integer.min(
+                            playersObs.contains(white1) ? playersObs.indexOf(white1) : Integer.MAX_VALUE,
+                            playersObs.contains(black1) ? playersObs.indexOf(black1) : Integer.MAX_VALUE
+                    );
+                    int minStr2 = Integer.min(
+                            playersObs.contains(white2) ? playersObs.indexOf(white2) : Integer.MAX_VALUE,
+                            playersObs.contains(black2) ? playersObs.indexOf(black2) : Integer.MAX_VALUE
+                    );
+                    if (minStr1 != minStr2) {
+                        return Integer.compare(minStr1, minStr2);
+                    } else {
+                        int higherPlayer1 = black1.getFideRating() > white1.getFideRating() ? playersObs.indexOf(black1) : playersObs.indexOf(white1);
+                        int higherPlayer2 = black2.getFideRating() > white2.getFideRating() ? playersObs.indexOf(black2) : playersObs.indexOf(white2);
 
 
-                    return Integer.compare(higherPlayer1, higherPlayer2);
+                        return Integer.compare(higherPlayer1, higherPlayer2);
+                    }
                 }
+
             }
         }
     }
