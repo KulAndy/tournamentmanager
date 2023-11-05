ebb3a882a76529a237694b3d3b501f8878d4f9b2
Andrzej Kulesza <andykrk22@gmail.com>
2023-11-05 22:45:58 +0100

    add player's card, fix result enter

---
+++ src/main/java/com/example/lolmanager/helper/players/PlayerCardHelper.java
@@ -0,0 +1,255 @@
+package com.example.lolmanager.helper.players;
+
+import com.example.lolmanager.model.Game;
+import com.example.lolmanager.model.Player;
+import com.example.lolmanager.model.Tournament;
+import javafx.beans.binding.DoubleBinding;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+
+public class PlayerCardHelper {
+    private Tournament tournament;
+    private ComboBox<Player> playerCardSelect;
+    private Label playerCardName;
+    private Label playerCardTB1;
+    private Label playerCardTB1Value;
+    private Label playerCardTB2;
+    private Label playerCardTB2Value;
+    private Label playerCardTB3;
+    private Label playerCardTB3Value;
+    private Label playerCardTB4;
+    private Label playerCardTB4Value;
+    private Label playerCardTB5;
+    private Label playerCardTB5Value;
+    private Label playerCardElo;
+    private Label playerCardEloValue;
+    private Label playerCardPZSzach;
+    private Label playerCardPZSzachValue;
+    private ListView<Game> playerCardGames;
+
+    public PlayerCardHelper(
+            Tournament tournament,
+            AnchorPane playerCard, ComboBox<Player> playerCardSelect,
+            Label playerCardName, Label playerCardTB1, Label playerCardTB1Value,
+            Label playerCardTB2, Label playerCardTB2Value, Label playerCardTB3, Label playerCardTB3Value,
+            Label playerCardTB4, Label playerCardTB4Value, Label playerCardTB5, Label playerCardTB5Value,
+            Label playerCardElo, Label playerCardEloValue, Label playerCardPZSzach, Label playerCardPZSzachValue,
+            ListView<Game> playerCardGames
+    ) {
+        setTournament(tournament);
+        setPlayerCardSelect(playerCardSelect);
+        getPlayerCardSelect().setCellFactory(param -> new ListCell<>() {
+            @Override
+            protected void updateItem(Player item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                } else {
+                    setText(item.getName());
+                }
+            }
+        });
+        getPlayerCardSelect().setButtonCell(new ListCell<>() {
+            @Override
+            protected void updateItem(Player item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                } else {
+                    setText(item.getName());
+                }
+            }
+        });
+        getPlayerCardSelect().setItems(getTournament().getPlayersObs());
+        setPlayerCardName(playerCardName);
+        setPlayerCardTB1(playerCardTB1);
+        getPlayerCardTB1().setText(String.valueOf(getTournament().getTiebreak().getTiebreak1()));
+        setPlayerCardTB1Value(playerCardTB1Value);
+        setPlayerCardTB2(playerCardTB2);
+        getPlayerCardTB2().setText(String.valueOf(getTournament().getTiebreak().getTiebreak2()));
+        setPlayerCardTB2Value(playerCardTB2Value);
+        setPlayerCardTB3(playerCardTB3);
+        getPlayerCardTB3().setText(String.valueOf(getTournament().getTiebreak().getTiebreak3()));
+        setPlayerCardTB3Value(playerCardTB3Value);
+        setPlayerCardTB4(playerCardTB4);
+        getPlayerCardTB4().setText(String.valueOf(getTournament().getTiebreak().getTiebreak4()));
+        setPlayerCardTB4Value(playerCardTB4Value);
+        setPlayerCardTB5(playerCardTB5);
+        getPlayerCardTB5().setText(String.valueOf(getTournament().getTiebreak().getTiebreak5()));
+        setPlayerCardTB5Value(playerCardTB5Value);
+        setPlayerCardElo(playerCardElo);
+        setPlayerCardEloValue(playerCardEloValue);
+        setPlayerCardPZSzach(playerCardPZSzach);
+        setPlayerCardPZSzachValue(playerCardPZSzachValue);
+        setPlayerCardGames(playerCardGames);
+
+        playerCard.prefWidthProperty().bind(getPlayerCardName().widthProperty());
+        playerCard.prefWidthProperty().bind(getPlayerCardGames().widthProperty());
+        playerCard.prefHeightProperty().bind(getPlayerCardGames().heightProperty());
+
+        getPlayerCardSelect().valueProperty().addListener((ObservableValue<? extends Player> observable, Player oldValue, Player newValue) ->{
+            if (newValue != null) {
+                getPlayerCardName().setText( "\t" + newValue.getTitle() + " " + newValue.getName());
+                getPlayerCardTB1Value().setText(newValue.getTiebreak(getTournament().getTiebreak().getTiebreak1()).toString());
+                getPlayerCardTB2Value().setText(newValue.getTiebreak(getTournament().getTiebreak().getTiebreak2()).toString());
+                getPlayerCardTB3Value().setText(newValue.getTiebreak(getTournament().getTiebreak().getTiebreak3()).toString());
+                getPlayerCardTB4Value().setText(newValue.getTiebreak(getTournament().getTiebreak().getTiebreak4()).toString());
+                getPlayerCardTB5Value().setText(newValue.getTiebreak(getTournament().getTiebreak().getTiebreak5()).toString());
+                getPlayerCardEloValue().setText(String.valueOf(newValue.getRatingPerformanceFide()));
+                getPlayerCardPZSzachValue().setText(String.valueOf(newValue.getRatingPerformancePZSzach()));
+
+                getPlayerCardGames().setItems(FXCollections.observableArrayList(newValue.getRounds()));
+            }
+        });
+    }
+
+    public Tournament getTournament() {
+        return tournament;
+    }
+
+    public void setTournament(Tournament tournament) {
+        this.tournament = tournament;
+    }
+
+    public ComboBox<Player> getPlayerCardSelect() {
+        return playerCardSelect;
+    }
+
+    public void setPlayerCardSelect(ComboBox<Player> playerCardSelect) {
+        this.playerCardSelect = playerCardSelect;
+    }
+
+    public Label getPlayerCardName() {
+        return playerCardName;
+    }
+
+    public void setPlayerCardName(Label playerCardName) {
+        this.playerCardName = playerCardName;
+    }
+
+    public Label getPlayerCardTB1() {
+        return playerCardTB1;
+    }
+
+    public void setPlayerCardTB1(Label playerCardTB1) {
+        this.playerCardTB1 = playerCardTB1;
+    }
+
+    public Label getPlayerCardTB1Value() {
+        return playerCardTB1Value;
+    }
+
+    public void setPlayerCardTB1Value(Label playerCardTB1Value) {
+        this.playerCardTB1Value = playerCardTB1Value;
+    }
+
+    public Label getPlayerCardTB2() {
+        return playerCardTB2;
+    }
+
+    public void setPlayerCardTB2(Label playerCardTB2) {
+        this.playerCardTB2 = playerCardTB2;
+    }
+
+    public Label getPlayerCardTB2Value() {
+        return playerCardTB2Value;
+    }
+
+    public void setPlayerCardTB2Value(Label playerCardTB2Value) {
+        this.playerCardTB2Value = playerCardTB2Value;
+    }
+
+    public Label getPlayerCardTB3() {
+        return playerCardTB3;
+    }
+
+    public void setPlayerCardTB3(Label playerCardTB3) {
+        this.playerCardTB3 = playerCardTB3;
+    }
+
+    public Label getPlayerCardTB3Value() {
+        return playerCardTB3Value;
+    }
+
+    public void setPlayerCardTB3Value(Label playerCardTB3Value) {
+        this.playerCardTB3Value = playerCardTB3Value;
+    }
+
+    public Label getPlayerCardTB4() {
+        return playerCardTB4;
+    }
+
+    public void setPlayerCardTB4(Label playerCardTB4) {
+        this.playerCardTB4 = playerCardTB4;
+    }
+
+    public Label getPlayerCardTB4Value() {
+        return playerCardTB4Value;
+    }
+
+    public void setPlayerCardTB4Value(Label playerCardTB4Value) {
+        this.playerCardTB4Value = playerCardTB4Value;
+    }
+
+    public Label getPlayerCardTB5() {
+        return playerCardTB5;
+    }
+
+    public void setPlayerCardTB5(Label playerCardTB5) {
+        this.playerCardTB5 = playerCardTB5;
+    }
+
+    public Label getPlayerCardTB5Value() {
+        return playerCardTB5Value;
+    }
+
+    public void setPlayerCardTB5Value(Label playerCardTB5Value) {
+        this.playerCardTB5Value = playerCardTB5Value;
+    }
+
+    public Label getPlayerCardElo() {
+        return playerCardElo;
+    }
+
+    public void setPlayerCardElo(Label playerCardElo) {
+        this.playerCardElo = playerCardElo;
+    }
+
+    public Label getPlayerCardEloValue() {
+        return playerCardEloValue;
+    }
+
+    public void setPlayerCardEloValue(Label playerCardEloValue) {
+        this.playerCardEloValue = playerCardEloValue;
+    }
+
+    public Label getPlayerCardPZSzach() {
+        return playerCardPZSzach;
+    }
+
+    public void setPlayerCardPZSzach(Label playerCardPZSzach) {
+        this.playerCardPZSzach = playerCardPZSzach;
+    }
+
+    public Label getPlayerCardPZSzachValue() {
+        return playerCardPZSzachValue;
+    }
+
+    public void setPlayerCardPZSzachValue(Label playerCardPZSzachValue) {
+        this.playerCardPZSzachValue = playerCardPZSzachValue;
+    }
+
+    public ListView<Game> getPlayerCardGames() {
+        return playerCardGames;
+    }
+
+    public void setPlayerCardGames(ListView<Game> playerCardGames) {
+        this.playerCardGames = playerCardGames;
+    }
+
+}
